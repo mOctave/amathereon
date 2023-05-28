9e2b19b73594d4a08cac0a9c7c18fb49e6c30016
mOctave <73318970+mOctave@users.noreply.github.com>
2023-05-28 16:44:54 -0700

    Add a couple of data items

--- amathereon/world/data/subscription_data.py
+++ amathereon/world/data/subscription_data.py
@@ -16,4 +16,35 @@ class Listings:
 		[1,"hillrockian_gold_mark","hillrockian_gold_penny","hillrockian_silver_penny","hillrockian_silver_fifthpenny"],
 	]
 
-	All = (HILLROCKIAN_CURRENCY,)
+	STANDARD_WEAPONS = [
+		"standard weapons",
+		[2,"sword"],
+		[2,"axe"],
+		[3,"spear"],
+		[2,"club"],
+		[4,"shield"],
+		[2,"longsword"],
+		[3,"shortsword"],
+		[1,"claymore"],
+		[2,"rapier"],
+		[4,"dagger"],
+		[1,"dagger","dagger"],
+		[2,"throwing knife"],
+		[2,"swordbreaker"],
+		[2,"sabre"],
+		[2,"battleaxe"],
+		[1,"greataxe"],
+		[2,"warhammer"],
+		[1,"bardiche"],
+		[3,"halberd"],
+		[2,"military fork"],
+		[2,"pike"],
+		[2,"partisan"],
+		[1,"scythe"],
+		[3,"javelin"],
+		[2,"quarterstaff"],
+		[1,"flail"],
+		[2,"mace"],
+	]
+
+	All = (HILLROCKIAN_CURRENCY,STANDARD_WEAPONS)
