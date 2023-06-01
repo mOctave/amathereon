d0730f234326dcb36e07f618c9ea7211ca8ab1c8
mOctave <73318970+mOctave@users.noreply.github.com>
2023-05-31 17:30:53 -0700

    Rename currency

--- amathereon/world/prototypes/currency.py
+++ amathereon/world/prototypes/currency.py
@@ -9,8 +9,8 @@ CURRENCY = {
 	"key": "currency"
 }
 
-CURRENCY_GOLD_CROWN_HILLROCKIA = {
-	"prototype_key": "CURRENCY_GOLD_CROWN_HILLROCKIA",
+HILLROCKIAN_GOLD_CROWN = {
+	"prototype_key": "HILLROCKIAN_GOLD_CROWN",
 	"prototype_parent": "CURRENCY",
 	"key": "hillrockian gold crown",
 	"desc": "This Hillrockian gold crown is not-quite pure gold, weighing about forty grains. It has been stamped with the head of some minor prince, which appears to be a tradition of the royal family. Worth: 40 gr gold.",
@@ -18,8 +18,8 @@ CURRENCY_GOLD_CROWN_HILLROCKIA = {
 	"mass": 40/16/120
 }
 
-CURRENCY_GOLD_MARK_HILLROCKIA = {
-	"prototype_key": "CURRENCY_GOLD_MARK_HILLROCKIA",
+HILLROCKIAN_GOLD_MARK = {
+	"prototype_key": "HILLROCKIAN_GOLD_MARK",
 	"prototype_parent": "CURRENCY",
 	"key": "hillrockian gold mark",
 	"desc": "This Hillrockian gold mark is slightly smaller than a gold crown, and worth about half as much. It is decorated with an intricate geometric design. Worth: 20 gr gold.",
@@ -27,8 +27,8 @@ CURRENCY_GOLD_MARK_HILLROCKIA = {
 	"mass": 20/16/80
 }
 
-CURRENCY_GOLD_PENNY_HILLROCKIA = {
-	"prototype_key": "CURRENCY_GOLD_PENNY_HILLROCKIA",
+HILLROCKIAN_GOLD_PENNY = {
+	"prototype_key": "HILLROCKIAN_GOLD_PENNY",
 	"prototype_parent": "CURRENCY",
 	"key": "hillrockian gold penny",
 	"desc": "This Hillrockian gold penny is the smallest unit of currency in Hillrockia when it comes to gold. It bears no design, being just a thick, small disk of pure gold. Worth: 5 gr gold.",
@@ -36,8 +36,8 @@ CURRENCY_GOLD_PENNY_HILLROCKIA = {
 	"mass": 5/16/480
 }
 
-CURRENCY_SILVER_PENNY_HILLROCKIA = {
-	"prototype_key": "CURRENCY_SILVER_PENNY_HILLROCKIA",
+HILLROCKIAN_SILVER_PENNY = {
+	"prototype_key": "HILLROCKIAN_SILVER_PENNY",
 	"prototype_parent": "CURRENCY",
 	"key": "hillrockian silver penny",
 	"desc": "This Hillrockian silver penny is used among Hillrockians for when even their smallest gold coins are too valuable. Worth: 1 gr gold.",
@@ -45,8 +45,8 @@ CURRENCY_SILVER_PENNY_HILLROCKIA = {
 	"mass": 4/16/480
 }
 
-CURRENCY_SILVER_FIFTHPENNY_HILLROCKIA = {
-	"prototype_key": "CURRENCY_SILVER_FIFTHPENNY_HILLROCKIA",
+HILLROCKIAN_SILVER_FIFTHPENNY = {
+	"prototype_key": "HILLROCKIAN_SILVER_FIFTHPENNY",
 	"prototype_parent": "CURRENCY",
 	"key": "hillrockian silver fifthpenny",
 	"desc": "This coin is, quite literally, a fifth of a Hillrockian silver penny. Worth: 0.2 gr gold.",
